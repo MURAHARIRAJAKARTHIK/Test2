c159fbd84c1a1ef632a385d5eefb0f778e0fe2fb
MUARAHARIRAJAKARTHIK <rajakrthik199613@gmail.com>
2025-08-03 12:25:31 +0530

    created setup for project

---
+++ task.js
@@ -0,0 +1,12 @@
+
+let grade = 85;
+
+if (grade >= 90) {
+  console.log("Grade: A");
+} else if (grade >= 80) {
+  console.log("Grade: B");
+} else if (grade >= 70) {
+  console.log("Grade: C");
+} else {
+  console.log("Grade: F");
+}
